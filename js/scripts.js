0dd67838c01f4f88073e8a990f15240e33c5d9e1
Afton Downey and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-13 08:45:07 -0800

    add js and css

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var answer = prompt("Would you like to learn about turtles, snakes, or insects? Write one of the three answers below:");
+
+  if (answer === "turtles") {
+    $("#turtles").show();
+  } else if (answer === "snakes") {
+      $("#snakes").show();
+    } else {
+        $("#snakes").show();
+      }
+});
